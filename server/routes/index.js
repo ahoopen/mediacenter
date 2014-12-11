0694318049a5382deb98661b01260d84cc87bd29
ahoopen <a.tenhoopen@gmail.com>
2014-12-11 23:35:21 +0100

    Fixed response error

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,6 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(request, response) {
     response.send('dit is een test');
+    response.end();
 });
 
 module.exports = router;
