3a63ed866b41e3f678beca22165a924fffc16f2f
ahoopen <a.tenhoopen@gmail.com>
2014-12-11 23:27:56 +0100

    Added express js and routes

--- server.js
+++ server.js
@@ -1,5 +1,31 @@
-var http = require('http');  
-http.createServer(function (req, res) {  
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello? Yes, this is Pi!\n');
-}).listen(1337);
+var express = require('express'),
+    http = require('http'),
+    routes = require('./server/routes/index');
+
+
+var app = express();
+
+// routes
+app.use('/', routes);
+
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+var port = Number(process.env.PORT || 1337);
+var server = http.createServer(app);
+var io = require('socket.io').listen(server);
+
+server.listen(port, function() {
+    console.log("Listening on " + port);
+});
+
+module.exports = app;
+
+//var http = require('http');
+//http.createServer(function (req, res) {
+//    res.writeHead(200, {'Content-Type': 'text/plain'});
+//    res.end('Hello? Yes, this is Pi!\n');
+//}).listen(1337);
