379f97195746d13a5625eb08e26b95136c4cea8b
ahoopen <a.tenhoopen@gmail.com>
2014-12-17 20:57:00 +0100

    Added function to get the duration of current playing file

--- server/controllers/player.js
+++ server/controllers/player.js
@@ -1,6 +1,9 @@
 /* globals module, require */
 var omx = require('omx-manager');
 
+var child_process = require('child_process');
+var exec = child_process.exec;
+
 module.exports = {
 
     init: function (io) {
@@ -14,9 +17,13 @@ module.exports = {
             socket.on('remote:start', function () {
                 console.log('play video');
                 omx.play(['/media/usb/newgirl.mkv'], {
-                    '-o' : 'hdmi'
+                    '-o' : 'hdmi',
+                    '-t' : true
                 });
-                omx.toggleSubtitles();
+
+                setInterval( getDuration('/media/usb/newgirl.mkv', function(time) {
+                    console.log(time);
+                }), 1000);
             });
 
             socket.on('remote:stop', function() {
@@ -24,4 +31,22 @@ module.exports = {
             });
         });
     }
+};
+
+var getDuration = function(video, callback) {
+    exec('omxplayer --info ' + video, function (err, stdout, stderr) {
+        if (!err) {
+            var duration = /(Duration:\s)([\d.:]+)/g.exec(stderr);
+            if (duration) {
+                var durationArray = duration[2].split(':');
+                var seconds = Math.ceil(durationArray[0]) * 60 * 60 + Math.ceil(+durationArray[1]) * 60 + Math.ceil(+durationArray[2]);
+                callback(seconds);
+            } else {
+                // couldn't find duration for some reason
+                callback(null);
+            }
+        } else {
+            callback(null);
+        }
+    });
 };
