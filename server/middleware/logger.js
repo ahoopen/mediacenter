c92ba21ae799ede1a94cf21b296eee37fbe3090b
ahoopen <a.tenhoopen@gmail.com>
2015-02-22 22:53:12 +0100

    Removed winston logging and removed epinfer from package json

--- server/middleware/logger.js
+++ server/middleware/logger.js
@@ -1,12 +1,4 @@
 /* globals require, module */
-var winston = require('winston');
-
-var logger = new (winston.Logger)({
-   transports: [
-       new (winston.transports.Console)({level: 'debug'}),
-       new (winston.transports.File)({ filename: './server/logs/api.log', level: 'debug'})
-   ]
-});
 
 module.exports = function (request, response, next) {
     logger.info("request received: [" + request.method + "] " + request.url);
